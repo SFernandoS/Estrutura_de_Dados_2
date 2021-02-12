61d1fe40aa261fab2461a1bd75f49ac64306d8b8
Fernando <fernando@DESKTOP-UI3V8O8.localdomain>
2021-02-12 04:03:47 -0300

    SelectionSort

---
+++ Algoritmos de Ordenacao/SelectionSort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define LENGTH 10
+#define MAX_NUM 100
+#define MIN_NUM 0
+
+void SelectionSort(int *array, int left, int right);
+bool less(int a, int b);
+void exch(int *a, int *b);
+
+
+int main(){
+    int array[LENGTH];
+    for(int i=0; i<LENGTH; i++){
+        array[i] = rand() % (MAX_NUM + 1 - MIN_NUM) + MIN_NUM;
+    }
+
+    SelectionSort(array, 0, LENGTH);
+
+    for(int i=0; i<LENGTH; i++)
+        printf("%d\n", array[i]);
+    
+    return 0;
+}
+
+void SelectionSort(int *array, int left, int right){
+    int min;
+
+    for(int i = left; i<LENGTH; i++){
+        min = i;
+        for(int j = i + 1; j < right; j++){
+            if(less(array[j],array[min]))
+                min = j;
+        }
+        exch(&array[min], &array[i]);
+    }
+}
+
+bool less(int a, int b){
+    return (a < b);
+}
+
+void exch(int *a, int *b){
+    int aux = *b;
+    *b = *a;
+    *a = aux;
+}
+
