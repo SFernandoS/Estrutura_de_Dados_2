cf249793022334943f6702210372168822c23457
Fernando <fernando@DESKTOP-UI3V8O8.localdomain>
2021-02-12 05:09:37 -0300

    BubbleSort

---
+++ Algoritmos de Ordenacao/BubbleSort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define LENGTH 10
+#define MAX_NUM 100
+#define MIN_NUM 0
+
+void bubbleSort(int *array, int left, int right);
+bool less(int a, int b);
+void exch(int *a, int *b);
+void cmpExch(int *a, int *b);
+
+
+int main(){
+    int array[LENGTH];
+    for(int i=0; i<LENGTH; i++){
+        array[i] = rand() % (MAX_NUM + 1 - MIN_NUM) + MIN_NUM;
+    }
+
+    bubbleSort(array, 0, LENGTH);
+
+    for(int i=0; i<LENGTH; i++)
+        printf("%d\n", array[i]);
+    
+    return 0;
+}
+
+void bubbleSort(int *array, int left, int right){
+
+    for(int i = left; i < right; i++)
+        for(int j = left; j < right; j++)
+            cmpExch(&array[j], &array[j+1]);
+}
+
+bool less(int a, int b){
+    return (a < b);
+}
+
+void exch(int *a, int *b){
+    int aux = *b;
+    *b = *a;
+    *a = aux;
+}
+
+void cmpExch(int *a, int *b){
+    if(less(*b, *a))
+        exch(a, b);
+}
+
